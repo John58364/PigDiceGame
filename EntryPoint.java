022fe7bd4c574c38532cbec39a92615324e68f08
JT Kelly <67845035+John58364@users.noreply.github.com>
2020-07-09 10:10:39 -0500

    Create EntryPoint.java

---
+++ EntryPoint.java
@@ -0,0 +1,17 @@
+/*
+* @JT Kelly
+* <p> EntryPoint
+* <p> Project 3
+* <p> 
+*/
+public class EntryPoint
+{
+
+	public static void main(String[] args)
+	{
+		// start up
+		Referee ref = new Referee();
+		ref.start();
+	}
+
+}
